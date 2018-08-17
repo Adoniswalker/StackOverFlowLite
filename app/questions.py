bef7888b989d8e9e58b5458e83e2af9f8a1bbdf3
dennisngeno <dennisngeno7@gmail.com>
2018-08-17 20:02:18 +0300

    [Feature #159785719] Add questions endpoints

---
+++ app/questions.py
@@ -0,0 +1,16 @@
+class Questions:
+    def __init__(self):
+        self.questions = [
+            {'id': 1, 'subject': 'What is js?',
+             "body": "Using tools that automate buildand testing when code is committed to a version control system",
+             'answers': ['Answer one', 'Answer two', 'Answer three']},
+            {'id': 2, 'subject': 'Is using APIs good?',
+             "body": "Creating API endpoints that will be consumed using Postman",
+             'answers': ['Answer one', 'Answer two', 'Answer three']},
+            {'id': 3, 'subject': 'What is tdd?', "body": "What is test driven development, i come across it yesterday",
+             'answers': ['Answer one', 'Answer two', 'Answer three']}]
+
+    def get_all_questions(self):
+        questions_list = [{'id': question['id'], 'subject': question['subject'],
+                           'body': question['body']} for question in self.questions]
+        return questions_list
--- app/views.py
+++ app/views.py
@@ -1,6 +1,13 @@
+from flask import jsonify, request
+
 from app import app
 
+from app.questions import Questions
 
-@app.route('/', )
-def new_user():
-    return "welcome to my apis"
+questions_object = Questions()
+
+
+@app.route('/api/v1/questions/', methods=['get'])
+def get_all_questions():
+    return jsonify(questions_object.get_all_questions())
+
