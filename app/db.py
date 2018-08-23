b6505b346fda9698b23ac46aeb7dcb5952cc4dd9
adoniswalker <dennisngeno7@gmail.com>
2018-08-23 11:31:36 +0300

    [Finished #159956132] Add signup endpoint

--- app/db.py
+++ app/db.py
@@ -1,24 +1,35 @@
-#!/usr/bin/env python3
+"""This file is used to setup and execute database commands"""
+# !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import os
 import json
+from urllib import parse
+
 import psycopg2
 import psycopg2.extras
 from flask import g
-from urllib import parse
 
 from app import app
 
 
 def load_row(row):
+    """
+    Change the data format
+    :param row:
+    :return: list
+    """
     loaded = {}
     for k, v in row.items():
-        loaded[k] = json.loads(v) if type(v) == str else v
+        loaded[k] = json.loads(v) if isinstance(str, v) else v
     return loaded
 
 
 def connect_db():
+    """
+    This will initialise connection
+    :return:
+    """
     parse.uses_netloc.append("postgres")
     if "DATABASE_URL" in os.environ:
         url = parse.urlparse(os.environ["DATABASE_URL"])
@@ -36,7 +47,7 @@ def connect_db():
 
 
 @app.teardown_appcontext
-def close_db(error):
+def close_db(errors=None):
     """Closes the database again at the end of the request
     if user is not logged in."""
     if hasattr(g, 'db_conn') and not hasattr(g, 'user'):
@@ -55,16 +66,23 @@ def get_db():
 
 
 def qry(query="", args=(), **kwargs):
+    """
+    This function is used to execute data queries and return data if needed
+    :param query:
+    :param args:
+    :param kwargs:
+    :return:
+    """
     conn = get_db()
     cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     cursor.execute(query, args)
-    if "commit" in kwargs and kwargs["commit"] == True:
+    if "commit" in kwargs and kwargs["commit"]:
         conn.commit()
     if "fetch" in kwargs and kwargs["fetch"] == "rowcount":
         return cursor.rowcount
     if "fetch" in kwargs and kwargs["fetch"] == "all":
         records = cursor.fetchall()
-        if "load" in kwargs and kwargs["load"] == True:
+        if "load" in kwargs and kwargs["load"]:
             loaded_records = []
             for row in records:
                 loaded_records.append(load_row(row))
@@ -72,7 +90,7 @@ def qry(query="", args=(), **kwargs):
         return records
     if "fetch" in kwargs and kwargs["fetch"] == "one":
         record = cursor.fetchone()
-        if "load" in kwargs and kwargs["load"] == True:
+        if "load" in kwargs and kwargs["load"]:
             record = load_row(record)
         if "attr" in kwargs:
             return record[kwargs["attr"]]
